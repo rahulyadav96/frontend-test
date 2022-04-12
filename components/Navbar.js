d399f8085c129216b5f1a9b02cb5b558ef678849
rahulyadav96 <rahul.yadav96@hotmail.com>
2022-04-12 17:29:52 +0530

    done with fake  login

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,22 @@
 
 import Link from "next/link";
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
-export const Navbar = ()=>{
+export const Navbar = () => {
+    const { auth, handleAuth } = useContext(AppContext);
+
     return (
         // <!-- This example requires Tailwind CSS v2.0+ -->
-        <nav className="bg-gray-800" style={{padding:"10px"}}>
-          <div  style={{color:"#fff", display:"flex", justifyContent:"space-around" }}>
-              <Link href="/">Home</Link>
-              <Link href="/keyData">Key data</Link>
-              <Link href="/login">Login</Link>
-          </div>
+        <nav className="bg-gray-800" style={{ padding: "10px" }}>
+            <div style={{ color: "#fff", display: "flex", justifyContent: "space-around" }}>
+                <Link href="/">Home</Link>
+                <Link href="/keyData">Key data</Link>
+                {
+                    auth ? <button onClick={handleAuth}>Logout</button> :
+                        <Link href="/login">Login</Link>
+                }
+            </div>
         </nav>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -82,9 +82,7 @@ export default function Example() {
           </li>
         ))}
       </ul>
-
-
-
+      <br />
       <div class="flex flex-col">
         <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
